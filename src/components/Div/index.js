0e03bd87dd2541461a579847d540cd607aab7222
Ankit Patel <ankitpatel4635@gmail.com>
2018-03-30 01:29:44 +0530

    add global css

--- src/components/Div/index.js
+++ src/components/Div/index.js
@@ -1,9 +1,44 @@
 import { styled, injectGlobal } from 'styled-components';
-import { normalize } from 'polished';
 
 /* eslint-disable */
+// styled-components usage
 injectGlobal`
-  ${normalize()}
+  /** Light **/
+  @font-face {
+  	font-family: 'SFPDLight';
+  	src: url('../../../static/fonts/light/SF-Pro-Display-Light.woff') format('woff');
+  	font-weight: normal;
+  	font-style: normal;
+  }
+
+  /** Regular **/
+  @font-face {
+  	font-family: 'SFPDRegular';
+  	src: url('../../../static/fonts/regular/SF-Pro-Display-Regular.woff') format('woff');
+  	font-weight: normal;
+  	font-style: normal;
+  }
+
+  /** Medium **/
+  @font-face {
+  	font-family: 'SFPDMedium';
+  	src: url('../../../static/fonts/medium/SF-Pro-Display-Medium.woff') format('woff');
+  	font-weight: normal;
+  	font-style: normal;
+  }
+
+  /** Semi Bold **/
+  @font-face {
+  	font-family: 'SFPDSemiBold';
+  	src: url('../../../static/fonts/semibold/SF-Pro-Display-Semibold.woff') format('woff');
+  	font-weight: normal;
+  	font-style: normal;
+  }
+
+  html, body {
+    font-family: 'SFPDRegular';
+    color: $text;
+  }
 `;
 /* eslint-enable */
 
--- src/container/index.js
+++ src/container/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Input from '../components/Inputs';
+import { Button, TomatoButton } from '../components/Buttons';
+import Div from '../components/Div';
 
 const Index = () => (
   <div>
+    <Div display="block">
+      <Button size="medium">Default</Button>
+      <TomatoButton size="medium">Default</TomatoButton>
+    </Div>
+    <Div display="flex">
+      <Button size="small" primary>Small with Primary</Button>
+      <Button size="medium">Medium</Button>
+    </Div>
+    <Button size="large">Large</Button>
+    <br />
     <Input />
   </div>
 );
