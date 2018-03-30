6afb0dcd84232616099fe33e2ff985df50444fe7
Ankit Patel <ankitpatel4635@gmail.com>
2018-03-30 13:15:34 +0530

    remove buttons

--- src/container/index.js
+++ src/container/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Input from '../components/Inputs';
-import { Button, TomatoButton } from '../components/Buttons';
+import { Button } from '../components/Buttons';
 import Div from '../components/Div';
 
 const Index = () => (
   <div>
     <Div display="block">
       <Button size="medium">Default</Button>
-      <TomatoButton size="medium">Default</TomatoButton>
     </Div>
     <Div display="flex">
       <Button size="small" primary>Small with Primary</Button>
