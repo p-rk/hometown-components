07aafdc69eb635672990e3e689e2f27f46f9682f
p-rk <rama.ped@gmail.com>
2018-04-26 15:58:51 +0530

    removing tests

--- src/__tests__/Div/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import 'jest-styled-components';
-import Div from '../../components/Div';
-
-test('It should return Div', () => {
-  const tree = renderer.create(<Div />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
