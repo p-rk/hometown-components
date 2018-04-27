547cd636f31924b594edd3496a5abe5dcc1e1b0f
p-rk <rama.ped@gmail.com>
2018-04-27 16:18:10 +0530

    base64 images

--- src/container/index.js
+++ src/container/index.js
@@ -11,6 +11,12 @@ import Input from 'components/Input';
 import Img from 'components/Img';
 import Container from 'components/Container';
 import Theme from 'components/Theme';
+
+
+import calendarImage from '../static/calendar.jpg';
+import creditCard from '../static/credit-card.jpg';
+import location from '../static/location.jpg';
+import truck from '../static/truck.jpg';
 // const = require('')
 
 const Index = () => (
@@ -24,7 +30,7 @@ const Index = () => (
         <Container>
           <Row display="block" mb="0.9375rem">
             <Div col="6">
-              <Img src="/static/location.png" />
+              <Img src={location} />
               <Label fontSize="1em" color="secondary" ml="0.625rem">Enter PIN Code</Label>
             </Div>
             <Div col="6">
@@ -37,7 +43,7 @@ const Index = () => (
           </Row>
           <Row display="block" mb="0.625rem">
             <Div col="12">
-              <Img src="/static/calendor.png" />
+              <Img src={calendarImage} />
               <Label
                 fontSize="1em"
                 color="secondary"
@@ -47,13 +53,13 @@ const Index = () => (
           </Row>
           <Row display="block" mb="0.625rem">
             <Div col="12">
-              <Img src="/static/credit-card.png" />
+              <Img src={creditCard} />
               <Label fontSize="1em" color="secondary" ml="0.625rem">EMI starting from Rs.2,419 </Label>
             </Div>
           </Row>
           <Row display="block" mb="0.625rem">
             <Div col="12">
-              <Img src="/static/truck.png" />
+              <Img src={truck} />
               <Label fontSize="1em" color="secondary" ml="0.625rem">Free Shipping</Label>
             </Div>
           </Row>
