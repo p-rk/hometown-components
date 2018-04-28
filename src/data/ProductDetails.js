e1ce96792264ae269c451253af40e14253da8aad
ankithometown <ankit.patel@hometown.in>
2018-04-28 17:21:41 +0530

    prodcut details json

---
+++ src/data/ProductDetails.js
@@ -0,0 +1,92 @@
+/* eslint-disable */
+const productDetails = {
+	"id": 1,
+	"name": "Martin Half Leather Single Seater Sofa",
+	"price": "65,000",
+	"disc_price": "62,000",
+	"saving": "3,000",
+	"percentage": "5%",
+	"rating": "5",
+	"tags": "#minimalist   #garcia   #oneseater   #bachelorfriendly",
+	"url": "http://via.placeholder.com/720x448/FFC720?text=Slide%201",
+	"deliver_by": "16 Jan (Sunday)",
+	"emi_starting": "2,419",
+	"description": "Add oodles of style to your home with an exciting range of designer furniture, furnishings, decor items and kitchenware. We promise to deliver best quality products at best prices.",
+	"images": [{
+			"image": "https://dt2czl74lk540.cloudfront.net/VUChVwc_-gBREU-D23PWHGEhMC8=/fit-in/780x446/http://assets.credr.com/bike_images/MH05CL0846_1.JPG",
+			"tag": "Bike image"
+		},
+		{
+			"image": "https://dt2czl74lk540.cloudfront.net/Qxjqpc7NQe6Ljy18G_b0pB1LR00=/fit-in/780x446/http://assets.credr.com/bike_images/MH05CL0846_2.JPG",
+			"tag": "Bike image"
+		},
+		{
+			"image": "https://dt2czl74lk540.cloudfront.net/-HOBhfevKFV-locj1bgj8Sk9aTI=/fit-in/780x446/http://assets.credr.com/bike_images/MH05CL0846_3.JPG",
+			"tag": "Bike image"
+		},
+		{
+			"image": "https://dt2czl74lk540.cloudfront.net/sFOajE554y6sBh8kU1GBKTednVs=/fit-in/780x446/http://assets.credr.com/bike_images/MH05CL0846_4.JPG",
+			"tag": "Bike image"
+		},
+		{
+			"image": "https://dt2czl74lk540.cloudfront.net/Vl76ug__lJ7ptuZ9BDloLmuph-s=/fit-in/780x446/http://assets.credr.com/bike_images/cc_1520672327682_a7b5hapjef8o9phufm2krhgpkm.JPG",
+			"tag": "Bike image"
+		}
+	],
+	"colors": [{
+			"hex": "#da2127"
+		},
+		{
+			"hex": "#000000"
+		},
+		{
+			"hex": "#DDDDDD"
+		},
+		{
+			"hex": "#F00"
+		}
+	],
+	"specs": [{
+		"Features": [{
+			"Design": "Standard",
+			"Assembly Required": "Yes"
+		}],
+		"Upholstery Material": [{
+				"Upholstery Material": "Microfiber"
+			},
+			{
+				"Upholstery Material Details": "Polyester"
+			}
+		],
+		"Weights & Dimensions": [{
+				"Overall": "30'' H x 36'' W x 30'' D"
+			},
+			{
+				"Seat": "15'' H'"
+			}
+		]
+	}],
+	"reviews": [{
+			"name": "Saurabh Suman",
+			"rating": "5",
+			"message": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo"
+		},
+		{
+			"name": "Deep Pathak",
+			"rating": "4",
+			"message": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo"
+		},
+		{
+			"name": "Rama",
+			"rating": "3",
+			"message": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo"
+		},
+		{
+			"name": "Ankit",
+			"rating": "3.5",
+			"message": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo"
+		}
+	]
+};
+
+export default productDetails;
