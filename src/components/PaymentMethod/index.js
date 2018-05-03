0b108dcb8be5714cba33afd797e3dc6fab2bdbca
ankithometown <ankit.patel@hometown.in>
2018-05-04 02:33:55 +0530

    payment method component changes

--- src/components/PaymentMethod/index.js
+++ src/components/PaymentMethod/index.js
@@ -9,7 +9,7 @@ import Text from 'components/Text';
 const visaIcon = require('../../static/visa.png');
 
 const PaymentMethod = () => (
-  <Section mb="0" p="0">
+  <Section mb="1.25rem" p="0">
     <Container type="container" pr="0" pl="0">
       <Row display="block" mr="0" ml="0" mb="0.3125rem">
         <Heading fontSize="1.25rem" color="textDark" mb="0px" mt="0px" fontFamily="SFPDLight">
@@ -18,7 +18,7 @@ const PaymentMethod = () => (
       </Row>
       <Row display="block" mr="0" ml="0" >
         <Text mb="0" mt="0.3125rem">
-          <Img src={visaIcon} alt="" /> ending in 9358
+          <Img src={visaIcon} alt="" float="left" mr="0.625rem" /> ending in 9358
         </Text>
       </Row>
     </Container>
