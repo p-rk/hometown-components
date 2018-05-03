3e1d7393368d24c397fdc0391c2bfd4d4df67fc7
ankithometown <ankit.patel@hometown.in>
2018-05-03 09:48:47 +0530

    create seprate component for review item

--- src/components/Reviews/index.js
+++ src/components/Reviews/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Div from 'components/Div';
-import { Label } from 'components/Label';
 import Heading from 'components/Heading';
-import Row from 'components/Row';
 import Container from 'components/Container';
-import Text from 'components/Text';
+import ReviewItem from 'components/Review/ReviewItem';
 
 const Reviews = ({ reviewItems }) => (
   <Container type="container" pr="0.5rem" pl="0.5rem">
@@ -14,15 +12,7 @@ const Reviews = ({ reviewItems }) => (
     </Heading>
     <Div display="block">
       {reviewItems.map(review => (
-        <Row display="block" m="0" mb="0.9375rem">
-          <Heading fontSize="0.875rem" color="secondary" mb="0.3125rem" mt="0px" fontFamily="SFPDMedium">
-            {review.name}
-          </Heading>
-          <Div>
-            <Label>{review.rating}/5</Label>
-          </Div>
-          <Text>{review.message}</Text>
-        </Row>
+        <ReviewItem review={review} />
       ))}
     </Div>
   </Container>
