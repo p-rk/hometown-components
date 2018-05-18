09aeaba41f099127c8a67b6a910a0fab7b3a06e2
ankithometown <ankit.patel@hometown.in>
2018-05-18 15:28:27 +0530

    color option change

--- src/components/ProductDetails/TitlePrice.js
+++ src/components/ProductDetails/TitlePrice.js
@@ -10,19 +10,19 @@ const TitlePrice = ({ prodDetails }) => (
   <Section mb="0.3125rem" p="0">
     <Container type="container" pr="1rem" pl="1rem">
       <Row display="block" mr="0" ml="0">
-        <Heading fontSize="1.25rem" color="textDark" mb="0.625rem" mt="0px" fontFamily="SFPDLight">
+        <Heading fontSize="1.25em" color="textDark" mb="0.75rem" mt="0px" fontFamily="SFPDLight">
           {prodDetails.name}
         </Heading>
-        <Heading fontSize="1rem" color="textDark" mb="0px" mt="0px" fontFamily="SFPDMedium">
+        <Heading fontSize="1em" color="textDark" mb="0px" mt="0px" fontFamily="SFPDMedium">
             Rs. {prodDetails.price}
           <Span
-            fontSize="0.8125rem"
+            fontSize="0.8125em"
             color="rgba(0, 0, 0, 0.4)"
             ml="1rem"
             fontFamily="SFPDMedium"
           >Rs. {prodDetails.disc_price}</Span>
           <Span
-            fontSize="0.8125rem"
+            fontSize="0.8125em"
             color="rgba(0, 0, 0, 0.4)"
             ml="1rem"
             fontFamily="SFPDMedium"
