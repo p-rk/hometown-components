eb6067611d13dcf74386cc5c88f25e1da72c4ffc
p-rk <rama.ped@gmail.com>
2018-07-11 18:31:52 +0530

    checking price and conditional rendering

--- src/components/ProductDetails/TitlePrice.js
+++ src/components/ProductDetails/TitlePrice.js
@@ -6,9 +6,9 @@ import Row from 'components/Row';
 import Span from 'components/Span';
 import Section from 'components/Section';
 
-const calculateDiscount = (price, discPrice) => ((price - discPrice) / price) * 100;
-
-const TitlePrice = ({ name, price, discPrice }) => (
+const TitlePrice = ({
+  name, price, discPrice, savingsRs, savingsPercentage
+}) => (
   <Section mb="0.3125rem" p="0">
     <Container type="container" pr="1rem" pl="1rem">
       <Row display="block" mr="0" ml="0">
@@ -17,21 +17,25 @@ const TitlePrice = ({ name, price, discPrice }) => (
         </Heading>
         <Heading fontSize="1em" color="textDark" mb="0px" mt="0px" fontWeight="600">
             Rs. {discPrice}
-          <Span
-            fontSize="0.8125em"
-            color="rgba(0, 0, 0, 0.4)"
-            ml="1rem"
-            fontWeight="600"
-            type="lt"
-          >Rs. {price}</Span>
-          <Span
-            fontSize="0.8125em"
-            color="rgba(0, 0, 0, 0.4)"
-            ml="1rem"
-            fontWeight="600"
-          >Saving Rs. {parseInt(price, 10) - parseInt(discPrice, 10) }
-           ({ Math.round(calculateDiscount(parseInt(price, 10), parseInt(discPrice, 10))) }%)
-          </Span>
+          { price !== discPrice &&
+            <span>
+              <Span
+                fontSize="0.8125em"
+                color="rgba(0, 0, 0, 0.4)"
+                ml="1rem"
+                fontWeight="600"
+                type="lt"
+              >Rs. {price}</Span>
+              <Span
+                fontSize="0.8125em"
+                color="rgba(0, 0, 0, 0.4)"
+                ml="1rem"
+                fontWeight="600"
+              >Saving Rs. {savingsRs}
+              ({savingsPercentage}% OFF)
+              </Span>
+            </span>
+          }
         </Heading>
       </Row>
     </Container>
@@ -41,13 +45,17 @@ const TitlePrice = ({ name, price, discPrice }) => (
 TitlePrice.propTypes = {
   name: PropTypes.string,
   price: PropTypes.string,
-  discPrice: PropTypes.string
+  discPrice: PropTypes.string,
+  savingsPercentage: PropTypes.string,
+  savingsRs: PropTypes.string
 };
 
 TitlePrice.defaultProps = {
   name: '',
   price: '',
   discPrice: '',
+  savingsPercentage: '',
+  savingsRs: ''
 };
 
 export default TitlePrice;
