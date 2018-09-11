dcaf6582458d9f0a33356725a2d58b2f5395662c
ankit <ankitpatel4635@gmail.com>
2018-09-11 17:37:45 +0530

    review page changes

--- src/components/ProductInlineWithQuantity/index.js
+++ src/components/ProductInlineWithQuantity/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import Link from 'components/Link';
-// import Button from 'components/Buttons';
-// import { Label } from 'components/Label';
 import Row from 'components/Row';
 import Div from 'components/Div';
 import Heading from 'components/Heading';
 import Img from 'components/Img';
 import Span from 'components/Span';
 import Text from 'components/Text';
+import ProgressiveImageSchemer from 'components/ProgressiveImageSchemer';
 import Theme from 'components/Theme';
+
+const Wrapper = styled(Div)`
+  background: #FFF;
+  padding: 0.5rem 0.5rem;
+  margin-bottom: 1rem;
+  border-radius: 3px;
+  -webkit-box-shadow: 0px 2px 5px 0px rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 2px 5px 0px rgba(0, 0, 0, 0.1);
+`;
 
 const calculateSavings = (cp, sp) => cp - sp;
 const getProductUrl = name => name.split(' ').join('-').split('%').join('');
@@ -23,53 +32,55 @@ const ProductInlineWithQuantity = ({
     }
   } = itemData;
   return (
-    <Div mr="0" ml="0" mb="1.375rem">
+    <Wrapper mr="0" ml="0" mb="1.375rem">
       {itemData && <Row display="block" mr="0" ml="0">
         <Link to={getProductUrl(name)}>
-          <Div col="4">
-            <Img
-              alt={name}
-              src={image}
-              width="100%"
-            />
+          <Div col="3">
+            <ProgressiveImageSchemer src={image} height="80px">
+              {imageURL => (<Img
+                src={imageURL}
+                alt={name}
+                width="100%"
+              />)}
+            </ProgressiveImageSchemer>
           </Div>
-          <Div col="8" pl="0.9375rem">
+          <Div col="9" pl="0.9375rem">
             <Heading
-              mb="0.4375rem"
+              mb="0"
               mt="0"
               color={Theme.colors.text}
-              fontFamily="medium"
-              fontSize="1.1rem"
-              lh="1.3"
+              fontFamily="regular"
+              fontSize="1rem"
+              lh="1.5"
             >{name}</Heading>
             <Div mb="0.25rem">
-              <Text mt="0" mb="0.3125rem">
+              <Text mt="0" mb="0" lh="1.4">
                 <Span
                   mr="0.625rem"
-                  fontSize="0.9rem"
+                  fontSize="0.875rem"
                   color={Theme.colors.textDark}
                   fontFamily="medium"
                 >Rs. {specialPrice}</Span>
                 <Span mr="0.625rem" fontSize="0.9rem">Rs. {unitPrice}</Span>
               </Text>
-              <Text mt="0" mb="0.3125rem">
+              <Text mt="0" mb="0" lh="1.4">
                 <Span
-                  fontSize="0.9rem"
+                  fontSize="0.75rem"
                 >Saving Rs. {calculateSavings(unitPrice, specialPrice) }({discount}%)</Span>
               </Text>
-              <Text mt="0" mb="0.3125rem">
+              <Text mt="0" mb="0" lh="1.4">
                 <Span
-                  fontSize="0.9rem"
+                  fontSize="0.75rem"
                 >Quantity {itemData.qty}</Span>
               </Text>
-              <Text mt="0" mb="0">
-                <Span fontSize="0.9rem">{shippingTime}</Span>
+              <Text mt="0" mb="0" lh="1.4">
+                <Span fontSize="0.875rem">{shippingTime}</Span>
               </Text>
             </Div>
           </Div>
         </Link>
       </Row>}
-    </Div>
+    </Wrapper>
   );
 };
 
