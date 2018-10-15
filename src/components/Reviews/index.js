d3937abf9b17c3a448ab34bd9b1456532c00c8a1
amarpathak <hi@apathak.com>
2018-10-15 21:53:50 +0530

    Reviews Sorting

--- src/components/Reviews/index.js
+++ src/components/Reviews/index.js
@@ -6,32 +6,36 @@ import Container from 'components/Container';
 import Section from 'components/Section';
 import ReviewItem from 'components/Reviews/ReviewItem';
 
-const Reviews = ({ reviewItems, col, pr }) => (
-  <Section
-    mb="0.3125rem"
-    p="0"
-    pt="0"
-    display="block"
-    mt="1rem"
-  >
-    <Container type="container" pl="1rem" pr="1rem">
-      <Heading
-        mb="1rem"
-        mt="0"
-        color="textLight"
-        fontSize="1em"
-        fontFamily="medium"
-      >
+const Reviews = ({ reviewItems, col, pr }) => {
+  reviewItems = reviewItems.sort((a, b) =>
+    (b.options[0] && Number(b.options[0].option_value)) - (a.options[0] && Number(a.options[0].option_value)));
+  return (
+    <Section
+      mb="0.3125rem"
+      p="0"
+      pt="0"
+      display="block"
+      mt="1rem"
+    >
+      <Container type="container" pl="1rem" pr="1rem">
+        <Heading
+          mb="1rem"
+          mt="0"
+          color="textLight"
+          fontSize="1em"
+          fontFamily="medium"
+        >
       Reviews
-      </Heading>
-      <Row display="block" m="0" mb="0">
-        {reviewItems.map((review, index) => (
-          <ReviewItem col={col} review={review} key={String(index)} pr={pr} />
-        ))}
-      </Row>
-    </Container>
-  </Section>
-);
+        </Heading>
+        <Row display="block" m="0" mb="0">
+          {reviewItems && reviewItems.map((review, index) => (
+            <ReviewItem col={col} review={review} key={String(index)} pr={pr} />
+          ))}
+        </Row>
+      </Container>
+    </Section>
+  );
+};
 
 Reviews.defaultProps = {
   col: '12',
