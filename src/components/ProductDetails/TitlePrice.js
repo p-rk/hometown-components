70dbef95a463083ab2374dea6f90d20349aa4c84
amarpathak <hi@apathak.com>
2018-10-19 16:28:34 +0530

    removed itemprop

--- src/components/ProductDetails/TitlePrice.js
+++ src/components/ProductDetails/TitlePrice.js
@@ -67,7 +67,6 @@ const TitlePrice = ({
               }
             </Heading>
             <Heading
-              itemProp="offers"
               itemScope
               itemType=""
               fontSize="1rem"
