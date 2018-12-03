bea96ce1886242cb5c05f6bdffceff38b1f763ce
ankit <ankitpatel4635@gmail.com>
2018-12-03 15:12:36 +0530

    SEO Changes

---
+++ src/components/HeadingH6/index.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+const Heading = styled.h6`
+  ${props => props.ellipsis && {
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis'
+  }}
+
+  font-size: ${props => props.fontSize};
+  font-family: ${props => props.fontFamily};
+  line-height: ${props => props.lh};
+  color: ${props => props.theme.colors[props.color]};
+
+  ${props => props.m && { margin: props.m }}
+  ${props => props.mt && { marginTop: props.mt }}
+  ${props => props.mr && { marginRight: props.mr }}
+  ${props => props.mb && { marginBottom: props.mb }}
+  ${props => props.ml && { marginLeft: props.ml }}
+  ${props => props.p && { padding: props.p }}
+  ${props => props.pt && { paddingTop: props.pt }}
+  ${props => props.pr && { paddingRight: props.pr }}
+  ${props => props.pb && { paddingBottom: props.pb }}
+  ${props => props.pl && { paddingLeft: props.pl }}
+  ${props => props.ls && { letterSpacing: props.ls }}
+  ${props => props.ta && { textAlign: props.ta }}
+  ${props => props.tt && { textTransform: props.tt }}
+  ${props => props.fontWeight && { fontWeight: props.fontWeight }}
+  ${props => props.display && { display: props.display }}
+`;
+
+Heading.defaultProps = {
+  fontSize: '1.5rem',
+  fontFamily: 'regular',
+  mt: '0.625em',
+  mb: '0.625em',
+  pb: '2px',
+  lh: '1.2',
+  color: 'primary',
+  ellipsis: true
+};
+
+export default Heading;
