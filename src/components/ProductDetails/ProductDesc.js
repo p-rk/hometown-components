f0e7ed5a930acace3fd1daab13397515f9a1e620
p-rk <rama.ped@gmail.com>
2018-12-04 12:29:11 +0530

    changes as per SEO

--- src/components/ProductDetails/ProductDesc.js
+++ src/components/ProductDetails/ProductDesc.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import Container from 'components/Container';
 import Row from 'components/Row';
 import Div from 'components/Div';
-import Heading from 'components/Heading';
+import HeadingH6 from 'components/HeadingH6';
 // import Text from 'components/Text';
 import Section from 'components/Section';
 import Button from 'components/Buttons';
@@ -38,12 +38,12 @@ const ProductDesc = ({ desc, showmore, toggleShowMore }) => {
       <Container type="container" pr="1rem" pl="1rem">
         <Row display="block" mb="0" mr="0" ml="0">
           <Div col="12" className={(desc.length > 120 && showmore) ? 'showLess' : 'showmore'}>
-            <Heading
+            <HeadingH6
               mt="0rem"
               color="textLight"
               fontSize="1em"
               fontFamily="light"
-            >Description</Heading>
+            >Description</HeadingH6>
             <Description
               itemProp="description"
               fontSize="0.875rem"
